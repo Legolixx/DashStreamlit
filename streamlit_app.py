54a80490c400ece4a5df8adf5bc00ce036cf997b
VICTOR <95864890+Legolixx@users.noreply.github.com>
2026-01-14 14:02:31 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,7 +44,7 @@ st.sidebar.title("Filtros")
 
 with st.expander("VER DADOS BRUTOS FILTRADOS"):
     st.dataframe(
-        df[['periodo', 'REGIAO', 'STATE']].sort_values(by='periodo', ascending=False)
+        df[['periodo', 'REGIÃO', 'STATE', 'GRUPO', 'DESCR_DEALER', 'titulo', 'realizado']].sort_values(by='periodo', ascending=False)
     )
 
 
